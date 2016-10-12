152c06814ad58649cb25eea4457bf183cc6c6a14
Chen, Hung <Hung.Chen@Pearson.com>
2016-10-12 18:11:50 -0400

    casing issues

--- plucky/web/js/api/projectApi.js
+++ plucky/web/js/api/projectApi.js
@@ -1,4 +1,4 @@
-import fetchWrap from './fetchwrapper';
+import fetchWrap from './fetchWrapper';
 
 export default {
 	buildProject: function(projectName, version) {
--- plucky/web/js/api/releaseApi.js
+++ plucky/web/js/api/releaseApi.js
@@ -1,4 +1,4 @@
-import fetchWrap from './fetchwrapper';
+import fetchWrap from './fetchWrapper';
 
 export default {
 	getRelease: function(projectName) {
